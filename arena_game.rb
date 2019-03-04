56adfe47ec986c14f59a0af8f15d5754ad2ee3ea
RhyG <rhysgeary@gmail.com>
2019-03-04 18:51:23 +1000

    Updated comments in main file.

--- arena_game.rb
+++ arena_game.rb
@@ -21,11 +21,11 @@ class Player
 
     # fight method for actual combat between player and chosen enemy
     def fight(enemy)
-        stored_health = enemy.health
-        5.times do # this generates a random number between the player level and the player attack then takes that from the enemy health
-            damage = (rand(1..self.weapon[:damage])) * self.attack
+        stored_health = enemy.health # stores the enemy health in a variable
+        5.times do # this generates a random number between 1 and the damage value of the equipped weapon, then takes that from the enemy health
+            damage = (rand(1..self.weapon[:damage])) * self.level
             puts "Dealt: #{damage} damage."
-            enemy.health -= damage
+            enemy.health -= damage # subtracts the damage from the enemy health
         end
         bar = ProgressBar.create(:title => "Fighting") # this creates a progress bar that 'simulates' the fight
         50.times { bar.increment; sleep 0.07 }
@@ -39,7 +39,7 @@ class Player
                 puts
                 self.level_up(5) # calls the level up method and passes it 'experience'
                 $enemies.delete(enemy) # deletes the champion from the array
-            elsif $practice_drones.include? enemy # 
+            elsif $practice_drones.include? enemy 
                 $coins_rewarded = 5
                 puts "The Tetrarchs award you" + " #{$coins_rewarded} coins".yellow + " for your efforts."
                 puts
@@ -72,6 +72,7 @@ class Player
         end
     end
 
+    # view stats method for printing the player stats, good for testing purposes
     def view_stats
         puts "Name: #{self.name}"
         puts "Level: #{self.level}"
@@ -80,15 +81,12 @@ class Player
         puts "Experience: #{self.experience}"
     end
 
-    def combat(hero, enemy)
-        return winner
-    end
-
+    # armoury method used to display weapons to the player and allow them to purchase them
     def armoury
         count = 0 # count allowing list to show background every second item
         puts "We've got the weapons, if you've got the coin."
         puts
-        $weapons.each do |weapon|
+        $weapons.each do |weapon| # iterates through array for display purposes. Gives every second weapon a white background.
             if count % 2 == 0
                 puts "Weapon: #{weapon[:name].capitalize} - Damage: #{weapon[:damage]} - Level: #{weapon[:level]} - Cost: #{weapon[:cost]}".black.on_white
             elsif weapon[:level] == 10
@@ -103,10 +101,10 @@ class Player
         print UI_ARROW.red + " " 
         user_armoury_choice = gets.chomp.downcase
         puts
-        $weapons.each do |weapon|
-            if user_armoury_choice == weapon[:name] && self.coins >= weapon[:cost]
 
-                dialogue_choice = rand(1...3)
+        $weapons.each do |weapon|
+            if user_armoury_choice == weapon[:name] && self.coins >= weapon[:cost] && self.level >= weapon[:level] # if user is a high enough level and has enough coins allows them to purchase weapon
+                dialogue_choice = rand(1...3) # used to select a different dialogue whenever purchasing a weapon
                 if dialogue_choice == 1
                     puts "That's a good piece, you've chosen well."
                 elsif dialogue_choice == 2
@@ -114,67 +112,68 @@ class Player
                 else 
                     puts "Used to own one of those myself, lost it in some chump's brain pan."
                 end
-
-                puts "#{weapon[:cost]} coins was subtracted from your coin pile."
-                self.weapon = weapon
-                self.coins -= weapon[:cost]
-                $weapons.delete(weapon)
-            elsif user_armoury_choice == weapon[:name] && self.coins < weapon[:cost]
+                puts "#{weapon[:cost]} coins was subtracted from your coin pile." 
+                self.weapon = weapon # assigns weapon to player
+                self.coins -= weapon[:cost] # takes weapon cost from player coins
+                $weapons.delete(weapon) # removes the weapon from the array
+            elsif user_armoury_choice == weapon[:name] && self.coins < weapon[:cost] # lets user know if they don't have enough coins
                 puts "You can't afford that, how 'bout you come back when you've got the funds?"
+            elsif user_armoury_choice == weapon[:name] && self.level < weapon[:level] # lets user know if they aren't a high enough level
+                puts "You haven't got the skill for a weapon like that."
+            else
+                puts "That's not an option."
             end
         end
         main_menu(self)         
     end
 
-    # def fight(player, enemy)
-    #     
-    # end
-
+    # choose enemy method used to display enemies, allow user to choose and fight an enemy
     def choose_enemy
         count = 0
         puts "Choose your adversary." 
         puts
         puts "Train your skills against the practice drones: "
-        $practice_drones.each do |practice_drone|
+        $practice_drones.each do |practice_drone| # used for display reasons, prints every second enemy on a white background
             if count % 2 == 0
                 puts "#{practice_drone.name.capitalize} - level: #{practice_drone.level}".black.on_white if (practice_drone.level < self.level + 3)
             else 
                 puts "#{practice_drone.name.capitalize} - level: #{practice_drone.level}" if (practice_drone.level < self.level + 3)
             end
+            count += 1
         end 
         puts
         puts "Choose a champion:"
-        count = 0
         $enemies.each do |enemy|
             puts "#{enemy.name.capitalize} - level: #{enemy.level}" if (enemy.level < self.level + 4)
         end
         puts
         enemy_choice = gets.chomp.downcase
        
-        enemy_found = false
+        enemy_found = false # boolean used to exit loop if an enemy is chosen
 
-        while enemy_found != true
-            $enemies.each do |enemy|
+        while enemy_found != true # while loop to display enemies if enemy_found is false
+            $enemies.each do |enemy| # prints the 'champions'
                 if enemy.name.include? enemy_choice
                     puts "Calling fight method"
-                    self.fight(enemy)
-                    enemy_found = true
+                    self.fight(enemy) # calls fight method and passes the chosen enemy
+                    enemy_found = true # sets enemy_found to true and exits the while loop
                 end
             end
-            $practice_drones.each do |practice_drone|
-                if practice_drone.name.include? enemy_choice #enemy_choice == practice_drone.name
+            $practice_drones.each do |practice_drone| # prints the 'practice drones'
+                if practice_drone.name.include? enemy_choice # checks to see if user entered the correct name of an enemy
                     puts "Entering arena"
-                    self.fight(practice_drone)
-                    enemy_found = true
+                    self.fight(practice_drone) # calls fight method and passes the chosen enemy
+                    enemy_found = true # sets enemy_found to true and exits the while loop
                 end
             end
-            enemy_found = true
+            enemy_found = true # sets enemy_found to true and exits the while loop
         end
         main_menu(self)
     end
 
     end
 
+    # prints out the available commands
     def list_commands
         puts UI_ARROW.light_yellow + " " + "'fight', 'f', or 'battle' to view enemies and fight."
         puts UI_ARROW.light_yellow + " " + "'armoury' or 'a' to go to the armoury."
@@ -182,22 +181,18 @@ class Player
         puts UI_ARROW.light_yellow + " " + "'clear' or 'cls' to clear the screen."
         puts UI_ARROW.light_yellow + " " + "'quit', 'q', or 'exit' to abandon your journey."
         puts
-
-        # lists available commands
     end
 
-    # also acts as the home area or 'hub'
+    # main menu, also acts as the home area
     def main_menu(player)
-        quit = false
-
-        while quit != true
+        quit = false # boolean used to exit menu loop
+        while quit != true # while loop to keep player in menu unless choice is made
         puts
         puts "Type 'commands' for a list of available commands."
         print UI_ARROW.red + " "
         input = gets.chomp.strip.downcase
         puts
-
-            case input
+            case input # switch statement to go to player menu choice
             when 'fight'
                 player.choose_enemy
             when 'armoury'
@@ -218,6 +213,8 @@ class Player
 
 
 system 'clear'
+
+# prints title card
 a = Artii::Base.new
 puts"                         WELCOME TO"
 puts a.asciify("THE  ARENA").black.on_white
--- enemies.rb
+++ enemies.rb
@@ -1,8 +1,3 @@
-# $enemies = [{name: "Priamus", level: 1, health: 10, attack: 1}, {name: "Duoro", level: 2, health: 20, attack: 3}, {name: "Tertius", level: 3, health: 40, attack: 5}, 
-#            {name: "Quattrus", level: 4, health: 80, attack: 8}, {name: "Quincetius", level: 5, health: 120, attack: 12}, {name: "Loken", level: 6, health: 150, attack: 15}, 
-#            {name: "Abbadon", level: 7, health: 200, attack: 20}, {name: "Erebus", level: 8, health: 250, attack: 28}, {name: "Lorgar", level: 9, health: 350, attack: 50}, 
-#            {name: "Warmaster Horus", level: 10, health: 500, attack: 65}]
-
 class Enemy
     attr_reader :name, :level
     attr_writer :is_practice_enemy
@@ -12,21 +7,15 @@ class Enemy
         @level = level
         @health = health
         @attack = attack
-        #@is_practice_enemy = false
     end
 end
 
 #(NAME, LEVEL, HEALTH, ATTACK)
-
 #practice bots
-slave_joe = Enemy.new("slavejoe", 1, 5, 1)
-#slave_joe.is_practice_enemy = true
+slave_joe = Enemy.new("slave dan", 1, 5, 1)
 unlucky_bob = Enemy.new("unlucky bob", 3, 20, 3)
-##unlucky_bob.is_practice_enemy = true
 mince = Enemy.new("Mince Meat", 6, 115, 15)
-#mince.is_practice_enemy = true
 greg = Enemy.new("Greg the Favoured One", 8, 220, 25)
-#greg.is_practice_enemy = true
 
 # champions
 priamus = Enemy.new("priamus the first son", 1, 10, 2)
