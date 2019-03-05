411c18e9c5c1787c17005694b931f90ddd6dda48
RhyG <rhysgeary@gmail.com>
2019-03-05 15:15:02 +1000

    Updated README.md

--- README.md
+++ README.md
@@ -6,13 +6,48 @@ LINK
 -ensure accessible to educators
 
 ### Description
-* Purpose
-* Functionality
-* Instructions for use
-* Screenshots
+
+The purpose of this app to take the user on a journey like never before. Sherine wanted to pretend she was a fierce warrior and so we built a role playing app allowing her to live out her fantasy.
+
+The Arena app is a simple, RPG battle game where the player trains against other people, levels up, earns money and buys new weapons all with the goal of defeating all 10 champions.
+
+### Functionality
+
+This app works through a number of different methods called from the player class. On launch the user creates a new player and is presented with a list of options. From there they can choose to select enemies and fight.
+The winner is determined by calculating a random number between 1 and their current weapon damage. This is then applied to the enemy health until all five rounds are complete. At the end of the five rounds, if the enemy has any health left
+the player loses, otherwise they emerge victorious and earn experience and coins.
+
+The player is also able to visit 'the armoury' from which they can view a list of weapons, and if they have enough coin and are a high enough level they can purchase new weapons increasing their damage.
+
+The game ends when all champions have been defeated.
+
+### Instructions for use
+
+On launch you will be prompted to give a name to your hero. Once you have done that you can view and choose menu options. There are practice bots you can fight to get started, and when you feel ready you can fight the champions. More champions will be revealed as you level up.
+
+After each battle you will earn coins and experience which you will need when you visit the armoury. In the armoury you can purchase new weapons and view their stats.
+
+Buy new gear and defeat all 10 champions to beat The Arena.
 
 ### Details of design and planning process inlcuding
 * Evidence of app idea brainstorming sessions
 * app User/Workflow diagram
 * Project plan and timeline
 * Screenshots of Trello board
+
+### Project timeline
+
+### Day 1:
+* Finalise idea
+* Implement core functionality
+* Build player class
+* Build enemy class
+* Work out fight method
+* Build main menu
+
+### Day 2: 
+* Extensive testing
+* Make everything pretty
+* Coins and levelling up features
+* Implement armoury feature
+* Implement gems
--- player_class.rb
+++ player_class.rb
@@ -11,9 +11,7 @@ class Player
     # initalizes the player with base stats
     def initialize(name)
         @name = name
-        @level = 1
         @health = 5
-        @attack = 1
         @weapon = $weapons[0]
         @wins = 0
         @coins = 0
@@ -43,8 +41,9 @@ class Player
                 self.level_up(5) # calls the level up method and passes it 'experience'
                 $enemies.delete(enemy) # deletes the champion from the array
             elsif $practice_drones.include? enemy 
-                $coins_rewarded = 5
+                $coins_rewarded = 5 * (0.5 * enemy.level)
                 puts "The Tetrarchs award you" + " #{$coins_rewarded} coins".yellow + " for your efforts."
+                exp = 2 * (enemy.level * 0.5)
                 self.level_up(2) # calls the level up method and passes it 'experience'
             else
             end
@@ -67,7 +66,6 @@ class Player
         self.experience
         if self.experience >= 10
             self.level += 1
-            self.attack += 2
             puts "You have reached level #{self.level}."
             self.experience = 0 # restore experience to zero on each level up
         end
@@ -123,8 +121,11 @@ class Player
             elsif user_armoury_choice == weapon[:name] && self.level < weapon[:level] # lets user know if they aren't a high enough level
                 puts "You haven't got the skill for a weapon like that."
             else
-                puts "That's not an option."
+                # puts "That's not an option."
             end
+        end
+        if ! $weapons.include? user_armoury_choice
+            puts "That isn't an option."
         end
         main_menu(self)         
     end
@@ -173,42 +174,4 @@ class Player
         end
         main_menu(self)
     end
-
 end
-
-# # prints out the available commands
-# def list_commands
-#     puts UI_ARROW.light_yellow + " " + "'fight', 'f', or 'battle' to view enemies and fight."
-#     puts UI_ARROW.light_yellow + " " + "'armoury' or 'a' to go to the armoury."
-#     puts UI_ARROW.light_yellow + " " + "'stats' to view your current status."
-#     puts UI_ARROW.light_yellow + " " + "'clear' or 'cls' to clear the screen."
-#     puts UI_ARROW.light_yellow + " " + "'quit', 'q', or 'exit' to abandon your journey."
-#     puts
-# end
-
-# # main menu, also acts as the home area
-# def main_menu(player)
-#     quit = false # boolean used to exit menu loop
-#     while quit != true # while loop to keep player in menu unless choice is made
-#     puts "Type 'commands' for a list of available commands."
-#     print UI_ARROW.red + " "
-#     input = gets.chomp.strip.downcase
-#     puts
-#         case input # switch statement to go to player menu choice
-#         when 'fight'
-#             player.choose_enemy
-#         when 'armoury'
-#             player.armoury
-#         when 'stats'
-#             player.view_stats
-#         when 'commands'
-#             list_commands
-#         when 'clear', 'cls' 
-#             system 'clear'
-#         when 'quit', 'q', 'exit'
-#             abort("You have abandoned your journey.".red)
-#         else
-#             puts "That's not an available command"
-#         end
-#     end
-# end
