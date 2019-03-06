5b421eb4de45a99f5ac7a45518c7b2a1cb1f2dcc
RhyG <rhysgeary@gmail.com>
2019-03-06 12:37:54 +1000

    Improved comments

--- main.rb
+++ main.rb
@@ -11,7 +11,7 @@ puts a.asciify("THE  ARENA").black.on_white
 UI_ARROW = "\u2712"
 print UI_ARROW.light_yellow
 puts " What is the name of your champion?"
-player = Player.new(gets.chomp)
+player = Player.new(gets.chomp) # creates new instance of player class with name entered by users
 puts UI_ARROW.light_yellow + " welcome to the arena, #{player.name}"
 
 main_menu(player)
